72efbf125e5ac9e553e8f3e10fb26587596bdb0a
rentryhard <rentryhard26@gmail.com>
2025-12-02 22:28:24 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: "/portofolio/",
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  }
 })
